fb59d0e2b0e122ec2b0a10091468904f46e2d4a5
Joey Gauthier <digitaljedi@outlook.com>
2019-08-18 11:23:04 -0500

    mocked out UI service to get basic JSON data for switching up UI

---
+++ src/services/info.service.js
@@ -0,0 +1,35 @@
+export default {
+  consoleInfo: () => {
+    return [
+      { name: 'PlayStation', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'PlayStation 2', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'NES', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'SNES', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'PlayStation 3', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Game Boy', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Game Boy Advance', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Sega Genesis', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Xbox', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Xbox 360', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Xbox One', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'PlayStation 4', igdbId: null, gbId: null, gbGuid: null },
+      { name: '3do', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Vita', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Philips CD-i', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Neo Geo Pocket', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Nintendo DS', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Atari 5200', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Atari Jaguar', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Atari Lynx', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'PSP', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Game Boy Color', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Sega Saturn', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'GameCube', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Wii', igdbId: null, gbId: null, gbGuid: null },
+      { name: 'Wii U', igdbId: null, gbId: null, gbGuid: null }
+    ];
+  },
+  fileInfo: () => {
+    return [];
+  }
+};
