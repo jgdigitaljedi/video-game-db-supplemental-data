c15f8a41bd56c7acc1f12a352f722c18e8214fe8
Joey Gauthier <digitaljedi@outlook.com>
2019-08-26 09:02:21 -0500

    setup created call for FileSearch and snackTime

--- src/components/FileSearch.vue
+++ src/components/FileSearch.vue
@@ -14,6 +14,7 @@
 
 <script>
 import JsonData from '../services/jsonData.service';
+import { mapMutations } from 'vuex';
 
 export default {
   name: 'FileSearch',
@@ -23,7 +24,24 @@ export default {
       filesList: null
     };
   },
-  created() {}
+  created() {
+    JsonData.getFile('static/fileInfoList.json')
+      .then(result => {
+        this.fileList - result.data;
+      })
+      .catch(error => {
+        console.log('file list ERROR', error);
+        this.snackTime({ status: 'error', txt: 'ERROR FETCHING FILE LIST!' });
+      });
+  },
+  methods: {
+    snackTime(snack) {
+      this.setSnack(snack);
+    },
+    ...mapMutations({
+      setSnack: 'setSnack'
+    })
+  }
 };
 </script>
 
