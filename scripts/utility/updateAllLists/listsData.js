3c1b497ddd2e783cb848dc4a541b632025b09245
Joey Gauthier <digitaljedi@outlook.com>
2022-05-27 07:17:03 -0500

    ran through a few misprints lists, more added to listData.js

--- scripts/utility/updateAllLists/listsData.js
+++ scripts/utility/updateAllLists/listsData.js
@@ -8,6 +8,7 @@ module.exports.listData = [
   {
     name: 'Magnavox Odyssey 2',
     id: 'ccl47',
+    prefix: 'mod',
     launchTitles: require(path.resolve(
       smallFiles,
       'launchTitles/magnavoxOdyssey2LaunchTitles.json'
@@ -20,6 +21,7 @@ module.exports.listData = [
   {
     name: 'Famicom Disk System',
     id: 'ccl48',
+    prefix: 'fds',
     launchTitles: null,
     exclusives: require(path.resolve(
       smallFiles,
@@ -32,6 +34,7 @@ module.exports.listData = [
   {
     name: 'Amiga',
     id: 'ccl49',
+    prefix: 'amig',
     launchTitles: null,
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/amigaExclusives.json')),
     misprintsAndErrors: null,
@@ -41,6 +44,7 @@ module.exports.listData = [
   {
     name: 'Nintendo Virtual Boy',
     id: 'ccl50',
+    prefix: 'nvb',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/virtualBoyLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/virtualBoyExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -53,6 +57,7 @@ module.exports.listData = [
   {
     name: 'Nintendo Entertainment System (NES)',
     id: 'ccl1',
+    prefix: 'nes',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/nesLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/nesExclusives.json')),
     misprintsAndErrors: null,
@@ -66,6 +71,7 @@ module.exports.listData = [
   {
     name: 'Super Nintendo Entertainment System (SNES)',
     id: 'ccl2',
+    prefix: 'snes',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/snesLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/nintendoSnesExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -81,6 +87,7 @@ module.exports.listData = [
   {
     name: 'Nintendo 64',
     id: 'ccl3',
+    prefix: 'n64',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/n64LaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/nintendoN64Exclusives.json')),
     misprintsAndErrors: null,
@@ -90,6 +97,7 @@ module.exports.listData = [
   {
     name: 'Nintendo GameCube',
     id: 'ccl4',
+    prefix: 'ngc',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/gamecubeLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/gamecubeExclusives.json')),
     misprintsAndErrors: null,
@@ -102,6 +110,7 @@ module.exports.listData = [
   {
     name: 'Nintendo Wii',
     id: 'ccl5',
+    prefix: 'wii',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/wiiLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/nintendoWiiExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -114,6 +123,7 @@ module.exports.listData = [
   {
     name: 'Nintendo Wii U',
     id: 'ccl6',
+    prefix: 'wiiu',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/wiiULaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/nintendoWiiUExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -126,6 +136,7 @@ module.exports.listData = [
   {
     name: 'Nintendo Switch',
     id: 'ccl7',
+    prefix: 'nsw',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/switchLaunchTitles.json')),
     exclusives: null,
     misprintsAndErrors: require(path.resolve(
@@ -138,6 +149,7 @@ module.exports.listData = [
   {
     name: 'Nintendo Game Boy',
     id: 'ccl8',
+    prefix: 'ngb',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/gameBoyLaunchTitles.json')),
     exclusives: require(path.resolve(
       smallFiles,
@@ -153,6 +165,7 @@ module.exports.listData = [
   {
     name: 'Nintendo Game Boy Color',
     id: 'ccl9',
+    prefix: 'ngbc',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/gameBoyColorLaunchTitles.json')),
     exclusives: require(path.resolve(
       smallFiles,
@@ -168,6 +181,7 @@ module.exports.listData = [
   {
     name: 'Nintendo Game Boy Advance',
     id: 'ccl10',
+    prefix: 'ngba',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/gameBoyAdvanceLaunchTitles.json')),
     exclusives: require(path.resolve(
       smallFiles,
@@ -180,6 +194,7 @@ module.exports.listData = [
   {
     name: 'Nintendo DS',
     id: 'ccl11',
+    prefix: 'nds',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/dsLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/nintendoDsExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -192,6 +207,7 @@ module.exports.listData = [
   {
     name: 'Nintendo 3DS',
     id: 'ccl12',
+    prefix: 'n3ds',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/nintendo3dsLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/nintendo3dsExclusives.json')),
     misprintsAndErrors: null,
@@ -201,6 +217,7 @@ module.exports.listData = [
   {
     name: 'Sega Master System',
     id: 'ccl13',
+    prefix: 'sms',
     launchTitles: require(path.resolve(
       smallFiles,
       'launchTitles/segaMasterSystemLaunchTitles.json'
@@ -219,6 +236,7 @@ module.exports.listData = [
   {
     name: 'Sega Mega Drive/Genesis',
     id: 'ccl14',
+    prefix: 'smd',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/genesisLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/segaGenesisExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -234,6 +252,7 @@ module.exports.listData = [
   {
     name: 'Sega CD',
     id: 'ccl15',
+    prefix: 'scd',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/segaCdLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/segaCdExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -246,6 +265,7 @@ module.exports.listData = [
   {
     name: 'Sega 32X',
     id: 'ccl16',
+    prefix: 's32x',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/sega32xLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/sega32xExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -258,6 +278,7 @@ module.exports.listData = [
   {
     name: 'Sega Saturn',
     id: 'ccl17',
+    prefix: 'ssat',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/segaSaturnLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/segaSaturnExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -270,12 +291,16 @@ module.exports.listData = [
   {
     name: 'Sega Dreamcast',
     id: 'ccl18',
+    prefix: 'sdc',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/dreamcastLaunchTitles.json')),
     exclusives: require(path.resolve(
       smallFiles,
       'platformExclusives/segaDreamcastExclusives.json'
     )),
-    misprintsAndErrors: null,
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/dreamcastMisprintsAndErrors.json'
+    )),
     special: [
       require(path.resolve(smallFiles, 'greatestHits/dreamcastJPAllStars.json')),
       require(path.resolve(smallFiles, 'greatestHits/dreamcastNAAllStars.json'))
@@ -285,6 +310,7 @@ module.exports.listData = [
   {
     name: 'Sega Game Gear',
     id: 'ccl19',
+    prefix: 'sgg',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/gameGearLaunchTitles.json')),
     exclusives: require(path.resolve(smallFiles, 'platformExclusives/segaGameGearExclusives.json')),
     misprintsAndErrors: require(path.resolve(
@@ -297,6 +323,7 @@ module.exports.listData = [
   {
     name: 'Sony PlayStation',
     id: 'ccl20',
+    prefix: 'psx',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/playstationLaunchTitles.json')),
     exclusives: require(path.resolve(
       smallFiles,
@@ -313,6 +340,7 @@ module.exports.listData = [
   {
     name: 'Sony Playstation 2',
     id: 'ccl21',
+    prefix: 'ps2',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/ps2LaunchTitles.json')),
     exclusives: require(path.resolve(
       smallFiles,
@@ -328,6 +356,7 @@ module.exports.listData = [
   {
     name: 'Sony Playstation 3',
     id: 'ccl22',
+    prefix: 'sps3',
     launchTitles: require(path.resolve(smallFiles, 'launchTitles/ps3LaunchTitles.json')),
     exclusives: require(path.resolve(
       smallFiles,
@@ -343,5 +372,173 @@ module.exports.listData = [
       require(path.resolve(smallFiles, 'greatestHits/sonyPlayStation3GreatestHits.json'))
     ],
     output: path.resolve(consoleLists, 'SonyPlayStation3.json')
+  },
+  {
+    name: 'Sony Playstation 4',
+    id: 'ccl23',
+    prefix: 'sps4',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/ps4LaunchTitles.json')),
+    exclusives: require(path.resolve(
+      smallFiles,
+      'platformExclusives/sonyPlaystation4Exclusives.json'
+    )),
+    misprintsAndErrors: null,
+    special: [require(path.resolve(smallFiles, 'greatestHits/sonyPlayStation4GreatestHits.json'))],
+    output: path.resolve(consoleLists, 'SonyPlayStation4.json')
+  },
+  {
+    name: 'Sony Playstation Portable',
+    id: 'ccl24',
+    prefix: 'spsp',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/pspLaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/sonyPspExclusives.json')),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/pspMisprintsAndErrors.json'
+    )),
+    special: [
+      require(path.resolve(smallFiles, 'greatestHits/sonyPlayStationPortableGreatestHits.json'))
+    ],
+    output: path.resolve(consoleLists, 'SonyPlayStationPortable.json')
+  },
+  {
+    name: 'Microsoft Xbox',
+    id: 'ccl25',
+    prefix: 'mxb',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/xboxLaunchTitles.json')),
+    exclusives: require(path.resolve(
+      smallFiles,
+      'platformExclusives/microsoftXboxExclusives.json'
+    )),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/xboxMisprintsAndErrors.json'
+    )),
+    special: [
+      require(path.resolve(smallFiles, 'backwardCompatibilityLists/XboxToXbox360.json')),
+      require(path.resolve(smallFiles, 'backwardCompatibilityLists/XboxToXboxOne.json')),
+      require(path.resolve(smallFiles, 'greatestHits/xboxClassicsPal.json')),
+      require(path.resolve(smallFiles, 'greatestHits/xboxPlatinumCollectionJP.json')),
+      require(path.resolve(smallFiles, 'greatestHits/xboxPlatinumHits.json')),
+      require(path.resolve(smallFiles, 'special/bannedInternationally.json'))
+    ],
+    output: path.resolve(consoleLists, 'MicrosoftXbox.json')
+  },
+  {
+    name: 'Microsoft Xbox 360',
+    id: 'ccl26',
+    prefix: 'x360',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/xbox360LaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/xbox360Exclusives.json')),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/xbox360MisprintsAndErrors.json'
+    )),
+    special: [
+      require(path.resolve(smallFiles, 'backwardCompatibilityLists/Xbox360ToXboxOne.json')),
+      require(path.resolve(smallFiles, 'special/bannedInternationally.json')),
+      require(path.resolve(smallFiles, 'greatestHits/xbox360ClassicsPal.json')),
+      require(path.resolve(smallFiles, 'greatestHits/xbox360PlatinumCollectionJP.json')),
+      require(path.resolve(smallFiles, 'greatestHits/xbox360PlatinumHits.json'))
+    ],
+    output: path.resolve(consoleLists, 'MicrosoftXbox360.json')
+  },
+  {
+    name: 'Microsoft Xbox One',
+    id: 'ccl27',
+    prefix: 'xone',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/xboxOneLaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/xboxOneExclusives.json')),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/xboxOneMisprintsAndErrors.json'
+    )),
+    special: [require(path.resolve(smallFiles, 'greatestHits/xboxOneGreatestHits.json'))],
+    output: path.resolve(consoleLists, 'MicrosoftXboxOne.json')
+  },
+  {
+    name: 'Atari 2600',
+    id: 'ccl28',
+    prefix: 'a260',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/atari2600LaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/atari2600Exclusives.json')),
+    misprintsAndErrors: null,
+    special: null,
+    output: path.resolve(consoleLists, 'Atari2600.json')
+  },
+  {
+    name: 'Atari 5200',
+    id: 'ccl29',
+    prefix: 'a520',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/atari5200LaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/atari5200Exclusives.json')),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/atari5200misprintsAndErrors.json'
+    )),
+    special: [require(path.resolve(smallFiles, 'multiplayer/atari5200FourPlayerGames.json'))],
+    output: path.resolve(consoleLists, 'Atari5200.json')
+  },
+  {
+    name: 'Atari 7800',
+    id: 'ccl30',
+    prefix: 'a780',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/atari7800LaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/atari7800Exclusives.json')),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/atari7800misprintsAndErrors.json'
+    )),
+    special: null,
+    output: path.resolve(consoleLists, 'Atari7800.json')
+  },
+  {
+    name: 'Atari Jaguar',
+    id: 'ccl31',
+    prefix: 'ajag',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/atariJaguarLaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/atariJaguarExclusives.json')),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/atariJaguarMisprintsAndErrors.json'
+    )),
+    special: null,
+    output: path.resolve(consoleLists, 'AtariJaguar.json')
+  },
+  {
+    name: 'Atari Lynx',
+    id: 'ccl32',
+    prefix: 'alnx',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/atariLynxLaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/atariLynxExclusives.json')),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/atariLynxMisprintsAndErrors.json'
+    )),
+    special: null,
+    output: path.resolve(consoleLists, 'AtariLynx.json')
+  },
+  {
+    name: 'NEC TurboGrafx 16',
+    id: 'ccl33',
+    prefix: 'tg16',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/turbografx16LaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/turbografx16Exclusives.json')),
+    misprintsAndErrors: require(path.resolve(
+      smallFiles,
+      'misprintsAndErrors/turboGrafxMisprintsAndErrors.json'
+    )),
+    special: [require(path.resolve(smallFiles, 'multiplayer/tg16Multitap.json'))],
+    output: path.resolve(consoleLists, 'TurboGrafx16.json')
+  },
+  {
+    name: 'NEC TurboGrafx CD',
+    id: 'ccl34',
+    prefix: 'tgcd',
+    launchTitles: require(path.resolve(smallFiles, 'launchTitles/turbografx16CdLaunchTitles.json')),
+    exclusives: require(path.resolve(smallFiles, 'platformExclusives/turbografxCdExclusives.json')),
+    misprintsAndErrors: null,
+    special: null,
+    output: path.resolve(consoleLists, 'TurboGrafxCD.json')
   }
 ];
