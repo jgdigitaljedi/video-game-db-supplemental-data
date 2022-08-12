211f713d0d7aec5adefb955223e2f72a0a7dae59
Joey Gauthier <digitaljedi@outlook.com>
2022-08-12 06:27:11 -0500

    added 2 pc engine launch titles for japan to tg16 launch titles list

--- scripts/utility/getPlatformInfo/getPlatformInfo.js
+++ scripts/utility/getPlatformInfo/getPlatformInfo.js
@@ -3,7 +3,7 @@ const gbController = require('../../../server/gb.controller');
 const gbKey = process.env.JGBKEY;
 const axios = require('axios');
 
-const pName = 'Odyssey';
+const pName = 'pc engine';
 
 const searchIgdbPlatform = async () => {
   try {
